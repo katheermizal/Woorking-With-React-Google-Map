d0f0609c1f8af55eb4cd1a3c0d5101a6e5e21974
Katheer Mizal <katheermizal@gmail.com>
2021-07-09 11:54:58 +0530

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{width: '100vw', height: '100vh'}}>
       <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBNTafGMXWcUCcqTOojeG7WlV14VX_2w8A`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=YOUR_API_KEY`}
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
